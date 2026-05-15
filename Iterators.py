f323b3d5b7fdeddcb9fc4e12c0c4b3b7dff72f8e
Sravya <mathinasravya@gmail.com>
2026-05-15 13:26:47 +0530

    Add files via upload

---
+++ Iterators.py
@@ -0,0 +1,12 @@
+
+lst = ['str1','str2',1,2,'hello'] # Iterable having list items
+tpl = (1,2,3,4,5) # Iterable having tuple items
+
+_iter_obj = iter(lst)
+print(_iter_obj)
+print(next(_iter_obj))
+print(next(_iter_obj))
+print(next(_iter_obj))
+print(next(_iter_obj))
+print(next(_iter_obj))
+print(next(_iter_obj))
